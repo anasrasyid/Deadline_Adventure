58c84b28e7c0ff67caddbd64e3495a4a079188e4
anasrasyid <anasrasyid333@gmail.com>
2019-11-30 14:39:17 +0700

    Obstacle 2
    Adding Asset

--- Assets/Script/Badut.cs
+++ Assets/Script/Badut.cs
@@ -4,10 +4,11 @@ public class Badut : Obstacle
     public GameObject pie;
     private GameObject pied;
     public Transform targets;
+    public Transform from;
 
     public override void Action()
     {
-        pied = Instantiate(pie, this.transform.position, Quaternion.identity) as GameObject;
+        pied = Instantiate(pie, from.position, Quaternion.identity) as GameObject;
         pied.SetActive(true);
         pied.GetComponent<Pie>().target = targets;
     }
@@ -23,5 +24,19 @@ public class Badut : Obstacle
         {
             Action();
         }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (audio == null)
+            gameObject.SetActive(false);
+        else
+            StartCoroutine(SoundExit());
+    }
+
+    public override void Activeable()
+    {
+        pied = null;
+        time = timer;
     }
 }
--- Assets/Script/Ghost.cs
+++ Assets/Script/Ghost.cs
@@ -3,11 +3,14 @@
 public class Ghost : Obstacle
 {
     public GameObject screen;
-    private float timeScreen = 1f;
+    public AudioClip actionSound;
+    private float timeScreen = 2f;
 
     public override void Action()
     {
         screen.SetActive(true);
+        audio.clip = actionSound;
+        audio.Play();
     }
 
     void Update()
@@ -21,8 +24,20 @@ public class Ghost : Obstacle
             if(timeScreen <= 0)
             {
                 screen.SetActive(false);
-                timeScreen = 1f;
+                timeScreen = 2.5f;
+                gameObject.SetActive(false);
             }
         }
+    }
+
+    private void OnMouseDrag()
+    {
+        StartCoroutine(SoundExit());
+    }
+
+    public override void Activeable()
+    {
+        time = timer;
+        EnterSound();
     }
 }
