d9d26999f51e789c9921b69dbbc6fcea3395d6bf
AlimanFZ <58335825+AlimanFZ@users.noreply.github.com>
2019-12-01 11:29:40 +0700

    Baru Cuk
    tambah Stage 5

--- Assets/Script/Ghost.cs
+++ Assets/Script/Ghost.cs
@@ -5,6 +5,12 @@ public class Ghost : Obstacle
     public GameObject screen;
     public AudioClip actionSound;
     private float timeScreen = 2f;
+    private Rigidbody2D rb2d;
+    private bool scared = false;
+    public float speedUp;
+
+    public Transform[] marker;
+    private int randomMarker;
 
     public override void Action()
     {
@@ -12,17 +18,27 @@ public class Ghost : Obstacle
         audio.clip = actionSound;
         audio.Play();
     }
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        randomMarker = Random.Range(0, marker.Length);    
+    }
 
     void Update()
     {
+        
+        
         if(DoTrouble() && !screen.active)
         {
             Action();
         }else if (screen.active)
         {
+            transform.position = marker[randomMarker].position;
+
             timeScreen -= Time.deltaTime;
             if(timeScreen <= 0)
             {
+
                 screen.SetActive(false);
                 timeScreen = 2.5f;
                 gameObject.SetActive(false);
@@ -40,4 +56,5 @@ public class Ghost : Obstacle
         time = timer;
         EnterSound();
     }
+ 
 }
--- Assets/Script/Notification.cs
+++ Assets/Script/Notification.cs
@@ -1,48 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Notification : Obstacle
 {
-    public GameObject popUp;
+    public float timer;
+    private float timeNow; 
+    public GameObject notif;
     public Sprite[] sprites;
+    public GameObject phone;
+    public GameObject NotifIcon;
+    public Text Pesan;
+    private bool phoneUp;
+    
+
+    
 
     private SpriteRenderer _renderer;
     public override void Action()
     {
         _renderer.sprite = sprites[1];
+
     }
 
     void Start()
     {
+        
         _renderer = GetComponent<SpriteRenderer>();
         Activeable();
+       
     }
 
     void Update()
     {
-        if (DoTrouble())
-        {
-            Action();
+            timer -= Time.deltaTime;
+            if (timer > 0 || notif.active)
+            {
+                notif.SetActive(true);
+                NotifIcon.SetActive(false);
+            }
+            else if(timer <= 0)
+            {
+                if(phone.active == false)
+                { 
+                    NotifIcon.SetActive(true);
+                    notif.SetActive(false);
+                }
         }
     }
-
-    private void OnMouseDrag()
+    public void NotifClick()
     {
-        if (_renderer.sprite == sprites[0])
+        if(NotifIcon.active)
         {
-            popUp.SetActive(true);
-            gameObject.SetActive(false);
+            notif.SetActive(true);
+            NotifIcon.SetActive(false);
         }
-        else
-        {
-            _renderer.sprite = sprites[0];
-        }
+    }
+    public void OnClickNotif()
+    {
+        timer = 0;
+        phone.SetActive(true);
+        notif.SetActive(false);
+        NotifIcon.SetActive(false);
         /*if (audio == null)
             gameObject.SetActive(false);
         else
             StartCoroutine(SoundExit());
         */
+    }
+    public void balas()
+    {
+        if(phone.active == true)
+        {
+            phone.SetActive(false);
+            NotifIcon.SetActive(false);
+        }
     }
 
     public override void Activeable()
