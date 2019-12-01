6b5dd254cd519947295009a5efbc0da58b8e596f
anasrasyid <anasrasyid333@gmail.com>
2019-12-01 08:58:18 +0700

    Disconflig

--- Assets/Anas/Script/Pie.cs
+++ Assets/Anas/Script/Pie.cs
@@ -21,6 +21,8 @@ public class Pie : MonoBehaviour
         float distCovered = (Time.time - startTime) * speed;
         float fractionJourney = distCovered / journeyLength;
         transform.position = Vector3.Lerp(transform.position, target.position,fractionJourney);
+
+        transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(0.5f,0.5f,0f),fractionJourney);
     }
 
     private void OnMouseDown()
